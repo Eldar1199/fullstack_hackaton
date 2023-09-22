447d0504cdec07fac668e66d494b5aacac9a63f1
Eldar1199 <usenov1199@gmail.com>
2023-09-22 13:07:55 +0600

    pull main

--- profilee/urls.py
+++ profilee/urls.py
@@ -3,5 +3,5 @@ from .views import ProfileUserAPIView, ProfileRecruiterAPIView
 
 urlpatterns = [
     path('user/', ProfileUserAPIView.as_view()),
-    path('req/', ProfileRecruiterAPIView.as_view()),
+    path('reс/', ProfileRecruiterAPIView.as_view()),
 ]
